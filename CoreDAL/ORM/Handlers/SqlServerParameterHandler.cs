a24156529d1e40574b1f74041be5338571a5065f
Jay9011 <whdtjq5904@gmail.com>
2025-12-10 16:42:45 +0900

    update. 파라미터 캐시화 추가

--- CoreDAL/ORM/Handlers/SqlServerParameterHandler.cs
+++ CoreDAL/ORM/Handlers/SqlServerParameterHandler.cs
@@ -7,11 +7,104 @@ using System.Reflection;
 using CoreDAL.ORM.Extensions;
 using CoreDAL.ORM.Interfaces;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CoreDAL.ORM.Handlers
 {
     public class SqlServerParameterHandler : IDbParameterHandler
     {
+        #region Parameter Cache
+
+        /// <summary>
+        /// 프로시저 파라미터 캐시
+        /// </summary>
+        private static readonly MemoryCache _parameterCache = new MemoryCache(new MemoryCacheOptions
+        {
+            SizeLimit = 1000
+        });
+
+        /// <summary>
+        /// 캐시 엔트리 옵션
+        /// - 슬라이딩 만료: 마지막 접근 후 30분
+        /// - 절대 만료: 최대 2시간
+        /// - 크기: 1 (프로시저 1개 = 1 단위)
+        /// </summary>
+        private static readonly MemoryCacheEntryOptions _cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSize(1)
+            .SetSlidingExpiration(TimeSpan.FromMinutes(30))
+            .SetAbsoluteExpiration(TimeSpan.FromHours(2))
+            .SetPriority(CacheItemPriority.Normal);
+
+        /// <summary>
+        /// 캐시 키 생성 (서버+DB+프로시저명 조합)
+        /// </summary>
+        /// <param name="connection">DB 연결</param>
+        /// <param name="procedureName">프로시저명</param>
+        /// <returns>캐시 키</returns>
+        private static string GetCacheKey(IDbConnection connection, string procedureName)
+        {
+            // 연결 문자열에서 서버와 DB를 추출하여 고유 키 생성
+            // 같은 프로시저라도 다른 서버/DB면 다른 캐시 엔트리
+            return $"{connection.ConnectionString.GetHashCode()}:{procedureName.ToUpperInvariant()}";
+        }
+
+        /// <summary>
+        /// SqlParameter 배열을 복제 (캐시된 객체 재사용 방지)
+        /// </summary>
+        /// <param name="source">원본 파라미터 배열</param>
+        /// <returns>복제된 파라미터 배열</returns>
+        private static SqlParameter[] CloneParameters(SqlParameter[] source)
+        {
+            var cloned = new SqlParameter[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                var src = source[i];
+                cloned[i] = new SqlParameter
+                {
+                    ParameterName = src.ParameterName,
+                    SqlDbType = src.SqlDbType,
+                    DbType = src.DbType,
+                    Direction = src.Direction,
+                    Size = src.Size,
+                    Precision = src.Precision,
+                    Scale = src.Scale,
+                    IsNullable = src.IsNullable,
+                    TypeName = src.TypeName,
+                    Value = DBNull.Value  // 값은 초기화
+                };
+            }
+            return cloned;
+        }
+
+        /// <summary>
+        /// 캐시 통계 조회 (디버깅/모니터링용)
+        /// </summary>
+        public static MemoryCacheStatistics GetCacheStatistics()
+        {
+            return _parameterCache.GetCurrentStatistics();
+        }
+
+        /// <summary>
+        /// 캐시 수동 제거 (특정 프로시저)
+        /// </summary>
+        /// <param name="connection">DB 연결</param>
+        /// <param name="procedureName">프로시저명</param>
+        public static void InvalidateCache(IDbConnection connection, string procedureName)
+        {
+            var cacheKey = GetCacheKey(connection, procedureName);
+            _parameterCache.Remove(cacheKey);
+        }
+
+        /// <summary>
+        /// 전체 캐시 초기화
+        /// </summary>
+        public static void ClearCache()
+        {
+            _parameterCache.Compact(1.0);  // 100% 압축 = 전체 제거
+        }
+
+        #endregion
+
         public string NormalizeParameterName(string paramName)
         {
             return paramName.TrimStart('@');
@@ -82,16 +175,60 @@ namespace CoreDAL.ORM.Handlers
             {
                 if (command is SqlCommand sqlCommand)
                 {
-                    var tmpCommand = new SqlCommand(sqlCommand.CommandText, connection as SqlConnection)
+                    var cacheKey = GetCacheKey(connection, sqlCommand.CommandText);
+
+                    // 캐시에서 조회 시도
+                    if (_parameterCache.TryGetValue(cacheKey, out SqlParameter[] cachedParameters))
+                    {
+                        // 캐시된 파라미터 복제본을 새 Command에 추가
+                        var clonedParams = CloneParameters(cachedParameters);
+                        var tmpCommand = new SqlCommand(sqlCommand.CommandText, connection as SqlConnection)
+                        {
+                            CommandType = CommandType.StoredProcedure,
+                            Transaction = sqlCommand.Transaction
+                        };
+
+                        foreach (var param in clonedParams)
+                        {
+                            tmpCommand.Parameters.Add(param);
+                        }
+
+                        return tmpCommand.Parameters;
+                    }
+
+                    // 캐시 미스: DB에서 파라미터 정보 조회
+                    var tmpCommandForDerive = new SqlCommand(sqlCommand.CommandText, connection as SqlConnection)
                     {
                         CommandType = CommandType.StoredProcedure,
                         Transaction = sqlCommand.Transaction
                     };
 
                     // 프로시저의 파라미터 정보를 가져옴
-                    SqlCommandBuilder.DeriveParameters(tmpCommand);
+                    SqlCommandBuilder.DeriveParameters(tmpCommandForDerive);
 
-                    return tmpCommand.Parameters;
+                    // 캐시에 저장 (파라미터 배열로 변환)
+                    var parametersToCache = new SqlParameter[tmpCommandForDerive.Parameters.Count];
+                    for (int i = 0; i < tmpCommandForDerive.Parameters.Count; i++)
+                    {
+                        var src = tmpCommandForDerive.Parameters[i];
+                        parametersToCache[i] = new SqlParameter
+                        {
+                            ParameterName = src.ParameterName,
+                            SqlDbType = src.SqlDbType,
+                            DbType = src.DbType,
+                            Direction = src.Direction,
+                            Size = src.Size,
+                            Precision = src.Precision,
+                            Scale = src.Scale,
+                            IsNullable = src.IsNullable,
+                            TypeName = src.TypeName,
+                            Value = DBNull.Value
+                        };
+                    }
+
+                    _parameterCache.Set(cacheKey, parametersToCache, _cacheEntryOptions);
+
+                    return tmpCommandForDerive.Parameters;
                 }
             }
             catch (Exception e)
